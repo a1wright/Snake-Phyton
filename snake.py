781592091f093c2f17b8743f41cbed992fbd733e
a1wright <ynolon@mail.ru>
2021-10-19 18:39:28 +0300

    Змейка c cчетом

--- snake.py
+++ snake.py
@@ -10,7 +10,8 @@ red = (255, 0, 0)
 black = (0, 0, 0)
  
 screen_width = 800
-screen_height = 600
+screen_height = 670
+work_screen_height = 600
  
 surface = pygame.display.set_mode((screen_width, screen_height)) # Инициализировать окно или экран для отображения
 pygame.display.set_caption('Змейка')                             # Заголовок текущего окна
@@ -34,11 +35,11 @@ def game_func():
  
     # Змейка
     x = screen_width / 2 
-    y = screen_height / 2 
+    y = work_screen_height / 2 
 
     # Еда
     food_x = round(random.randrange(0,screen_width  - 20) / 20.0) * 20.0
-    food_y = round(random.randrange(0,screen_height - 20) / 20.0) * 20.0
+    food_y = round(random.randrange(0,work_screen_height - 20) / 20.0) * 20.0
 
     # Переменные для сохранения обновленных координат
     x_new = 0                                                  
@@ -107,7 +108,7 @@ def game_func():
                         game_func()                                     
 
         # Змейка коснулась границы => Конец игры
-        if x >= screen_width or x < 0 or y >= screen_height or y < 0:
+        if x >= screen_width or x < 0 or y >= work_screen_height or y < 0:
             game_close = True
 
         x += x_new
@@ -132,6 +133,11 @@ def game_func():
  
         upd_snake(snake_block, snake_list)
 
+        # Cчет
+        pygame.draw.rect(surface, white,(0, work_screen_height, screen_width, 20))
+        score_font = pygame.font.SysFont(None, 35)
+        value = score_font.render("Счет: " + str(snake_Length - 1) , True, green)
+        surface.blit(value, [10, screen_height-40])
 
            
                                                        
@@ -140,7 +146,7 @@ def game_func():
         # Обновление координат еды => увеличение длины змейки
         if x == food_x and y == food_y:
             food_x = round(random.randrange(0, screen_width - snake_block) / 20.0) * 20.0
-            food_y = round(random.randrange(0, screen_height - snake_block) / 20.0) * 20.0
+            food_y = round(random.randrange(0, work_screen_height - snake_block) / 20.0) * 20.0
             snake_Length += 1
       
         # ограничение fps
